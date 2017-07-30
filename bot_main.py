2080649e1744826782100066970d6bc0c37ad45f
Mohamed Salem <gitexel@users.noreply.github.com>
2017-07-30 20:24:11 +0300

    new futures
    store all id scans to a local file.
    store last max id cursor to a local file.
    Add sleep mode for waiting for the Instagram server response.
    Sleep mode.
    follower min and max.

--- bot_main.py
+++ bot_main.py
@@ -3,45 +3,16 @@ import re
 import requests as req
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import time
 
-print "Welcome to Hjelleset Bot"
-tag = raw_input("Please Enter your HashTag name: ")
-print "Please Wait..."
-unique_id_list = []
 
-""" google spreadsheet """
 
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-headers = gspread.httpsession.HTTPSession(headers={'Connection': 'Keep-Alive'})  # increase session timeout
-client = gspread.Client(auth=credentials, http_session=headers)
-client.login()
-
-google_sheet_name = "hjelleset_mining"
-Tags_sheet = client.open(google_sheet_name).sheet1
-tag_lists = Tags_sheet.col_values(1)
-
-if tag not in tag_lists:
-    Tags_sheet.append_row([tag])
-    new_sheet = client.open(google_sheet_name).add_worksheet(tag, 1, 6)
-    new_sheet.insert_row(
-        ["ID", "Username", "Full Name", "Followers count",
-         "Email Address", "LastMaxID"], 1)
-    new_sheet.delete_row(2)
-else:
-    """Local files - unique ids """
-    try:
-        with open("guests_uniqueIDs/" + tag + ".txt", "rb") as fp:  # Unpickling
-            unique_id_list = pickle.load(fp)
-    except e:
-        print e
-
-sheet = client.open(google_sheet_name).worksheet(tag)
+def refresh_token():
+    if credentials.access_token_expired:  # refreshes the token
+        client.login()
 
 
 def human_info(guest_user_id):
-    if credentials.access_token_expired:  # refreshes the token
-        client.login()
     full_url = "https://i.instagram.com/api/v1/users/" + guest_user_id + "/info/"
     return req.get(full_url).json()
 
@@ -67,11 +38,13 @@ def user_name(human_info_json):
 
 
 def hash_tag_mining():
-
     try:
 
-        max_id = ''
-        url = "https://www.instagram.com/explore/tags/" + tag + "/?__a=1"
+        max_id = Max_id_cursor
+        if max_id != '':
+            url = "https://www.instagram.com/explore/tags/" + tag + "/?__a=1" + "&max_id=" + str(max_id)
+        else:
+            url = "https://www.instagram.com/explore/tags/" + tag + "/?__a=1"
 
         tag_json = req.get(url).json()
 
@@ -84,38 +57,87 @@ def hash_tag_mining():
                 human_id = item["owner"]["id"]
 
                 if human_id not in unique_id_list:
-
                     info_json = human_info(human_id)
-
                     if info_json["status"] == "ok":
-
                         unique_id_list.append(human_id)
-                        if 1000 <= followers_count(info_json) <= 50000:
+                        followers_num = followers_count(info_json)
+                        if followers_min <= followers_num and followers_num <= followers_max:
                             row = [human_id, user_name(info_json), full_name(info_json), followers_count(info_json),
-                                   email_address(info_json), max_id]
-                            if credentials.access_token_expired:  # refreshes the token
-                                client.login()
+                                   email_address(info_json)]
+                            refresh_token()
                             sheet.append_row(row)
-                            max_id = ''
                     else:
-                        print info_json["status"], "requsts"
-                        break
+                        count = 0.0
+                        print info_json["status"], ":", info_json["message"]
+                        print "Waitng the server to response with ok message..."
+                        print "Sleep mode on"
+                        while (human_info(human_id)["status"] == "fail"):
+                            time.sleep(10)
+                            count += 1
+                        print "Sleep mode off,", "Waiting time:", (count * 10.0) / 60.0, "minute(s)"
 
             if tag_json["tag"]["media"]["count"] > 15 and tag_json["tag"]["media"]["page_info"]["has_next_page"]:
-                if credentials.access_token_expired:  # refreshes the token
-                    client.login()
                 max_id = tag_json["tag"]["media"]["page_info"]["end_cursor"]
                 url_next_page = "https://www.instagram.com/explore/tags/" + tag + "/?__a=1" + "&max_id=" + str(max_id)
                 tag_json = req.get(url_next_page).json()
-
+                with open("last_page_max_id/" + tag + "_max_id_list.txt", "wb") as fp:  # Pickling
+                    pickle.dump(max_id, fp)
+                with open("guests_uniqueIDs/" + tag + ".txt", "wb") as fp:  # Pickling
+                    pickle.dump(unique_id_list, fp)
             else:
                 has_next_page = False
-
     except Exception as e:
-        print(e)
+        print e
 
+
+print "Welcome to Hjelleset Bot"
+print "The bot helps you gathring Instagram users info debends on the hashtag mining."
+tag = raw_input("Please Enter your HashTag name: ")
+followers_min = int(raw_input("Enter the Minumum number of follower: "))
+followers_max = int(raw_input("Enter the Maximum number of follower: "))
+
+if followers_min > followers_max or followers_min == 0 or followers_max == 0:
+    print "Invalid inputs"
+    exit(1)
+
+print "Please Wait..."
+unique_id_list = []
+Max_id_cursor = ""
+
+""" google spreadsheet scope"""
+
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+headers = gspread.httpsession.HTTPSession(headers={'Connection': 'Keep-Alive'})  # increase session timeout
+client = gspread.Client(auth=credentials, http_session=headers)
+client.login()
+
+google_sheet_name = "hjelleset_mining"
+Tags_sheet = client.open(google_sheet_name).sheet1
+tag_lists = Tags_sheet.col_values(1)
+
+# find if the tag exist or not
+if tag not in tag_lists:
+    Tags_sheet.append_row([tag])
+    new_sheet = client.open(google_sheet_name).add_worksheet(tag, 1, 5)
+    new_sheet.insert_row(
+        ["ID", "Username", "Full Name", "Followers count",
+         "Email Address"], 1)
+    new_sheet.delete_row(2)
+else:
+    try:
+        with open("guests_uniqueIDs/" + tag + ".txt", "rb") as fp:  # Unpickling
+            unique_id_list = pickle.load(fp)
+    except Exception as e:
+        print e
+    try:
+        with open("last_page_max_id/" + tag + "_max_id_list.txt", "rb") as fp:  # Unpickling
+            Max_id_cursor = pickle.load(fp)
+    except Exception as e:
+        print e
+
+sheet = client.open(google_sheet_name).worksheet(tag)
 
 hash_tag_mining()
 
-with open("guests_uniqueIDs/" + tag + ".txt", "wb") as fp:  # Pickling
-    pickle.dump(unique_id_list, fp)
+print "Successfully mining."
